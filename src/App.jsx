d550a78b64cbd7844812a47b15d0f787c4ece197
alchemyyt <josemontesdeoca00@gmail.com>
2024-12-27 08:44:09 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-white">Hola, soy José Montes de Oca</h1>
+      <p>
+        Un desarrollador full-stack con experiencia en JavaScript. Me encanta
+        transformar ideas en realidad a través del desarrollo web.
+      </p>
+      <h2>Las tecnologias que suelo usar</h2>
+      <ul>
+        <li>React</li>
+        <li>Tailwind CSS</li>
+        <li>Vite</li>
+        <li>Next.js</li>
+        <li>MongoDB</li>
+        <li>PayloadCMS</li>
+        <li>Node.js</li>
+        <li>React Native</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
